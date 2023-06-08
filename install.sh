8a999d0ccdd2e63ec5767dbd9d004c8d3d5ef4d4
PeterLiu-all <peterliuforever@gmail.com>
2023-06-08 17:42:11 +0800

    improve experience

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 sudo cp ./viman /usr/local/bin/
 sudo chmod a+x /usr/local/bin/viman
-echo 'function! Viman(entry_name)
-    tabnew | execute "term ++curwin viman -e " . a:entry_name
+echo 'function! VimanFunc(entry_name, ...)
+    let args = join(a:000, ' ')
+    tabnew | execute "term ++curwin viman -e " . a:entry_name . " " . args
 endfunction
-cnoreabbrev viman call Viman("")<left><left>' >> $HOME/.vimrc
+
+command! -nargs=+ Viman :call VimanFunc(<f-args>)
+cnoreabbrev viman Viman' >> $HOME/.vimrc
